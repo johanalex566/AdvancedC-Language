d6249ddb8a9abac19a020c66d7427dc18606b062
Johan Sanchez <johan.sanchez@ghtcorp.com>
2024-07-25 19:19:05 -0500

    Add project files.

---
+++ WarehouseManagementSystem.Business/OrderProcessor.cs
@@ -0,0 +1,29 @@
+﻿using WarehouseManagementSystem.Domain;
+
+namespace WarehouseManagementSystem.Business
+{
+    public class OrderProcessor
+    {
+        //permitirá hacer referencia a un método que se ejecutará cuando se inicialice la orden
+        public delegate void OrderInitialized();
+        public delegate void ProcessCompleted();
+
+        public OrderInitialized OnOrderInitialized { get; set; }
+
+
+        private void Initialize(Order order)
+        {
+            OnOrderInitialized?.Invoke();
+        }
+
+        public void Process(Order order, ProcessCompleted onCompleted = default )
+        {
+            // Run some code..
+
+            Initialize(order);
+
+            onCompleted?.Invoke();
+            // How do I produce a shipping label?
+        }
+    }
+}
---
+++ WarehouseManagementSystem/Program.cs
@@ -0,0 +1,40 @@
+﻿using WarehouseManagementSystem.Business;
+using WarehouseManagementSystem.Domain;
+
+    /*
+     *Queremos proporcionar a los consumidores de la capa empresarial
+     *Una forma de ejecutar su propio código cuando se inicialice un pedido
+     *y cuando se complet el procesamiento del pedido.
+     *
+     *Un delegado es un tipo que representa referencias a métodos.
+     *Se usa cuando necesitas un callback o recibir una devolución de llamada cuandose completea una operación.
+     *
+     */
+
+var order = new Order
+{
+    LineItems = new[] {
+    new Item { Name ="PS1", Price =50},
+    new Item { Name ="PS2", Price =60},
+    new Item { Name ="PS3", Price =70},
+    new Item { Name ="PS4", Price =80}
+    }
+
+};
+
+var processor = new OrderProcessor();
+
+processor.OnOrderInitialized = SendMessageToWarehouse; //Estamos señalando el método para delegar el trabajo
+
+processor.Process(order, SendConfirmationEmail);
+
+
+void SendMessageToWarehouse()
+{
+    Console.WriteLine("Please pack the order");
+}
+
+void SendConfirmationEmail()
+{
+    Console.WriteLine("Order Confirmation Email");
+}
